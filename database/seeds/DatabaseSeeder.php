9d8294f2cf24517f3f9cd8f610df13392417a100
alumne <alexbonavila@iesebre.com>
2016-05-22 17:21:19 +0200

    creats models i migracions per a tags i tasks i tambe instalat el llu

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Tag;
+use App\Task;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+        // $this->call(UserTableSeeder::class);
+        $faker = Faker\Factory::create();
+        $this->seedTasks($faker);
+        $this->seedTags($faker);
+        Model::reguard();
+    }
+    /**
+     * @param Faker $faker
+     */
+    private function seedTasks($faker)
+    {
+        foreach (range(0,100) as $item) {
+            $task = new Task();
+            $task->name = $faker->sentence();
+            $task->done = $faker->boolean();
+            $task->priority = $faker->randomDigit();
+            $task->save();
+        }
+    }
+    private function seedTags($faker)
+    {
+        foreach (range(0,100) as $item) {
+            $tag = new Tag();
+            $tag->name = $faker->word;
+            $tag->save();
+        }
     }
 }
